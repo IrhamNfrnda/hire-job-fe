4700336003bd416984531d4bc67c172d6533667c
IrhamNfrnda <inofrianda@gmail.com>
2023-07-28 06:40:15 +0700

    [ADD] Icon

--- components/Navigations.js
+++ components/Navigations.js
@@ -4,27 +4,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import axios from "axios";
 
 function Navigations() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userData, setUserData] = React.useState("")
 
   useEffect(() => {
     setIsAuthenticated(localStorage.getItem("auth") !== null);
   }, []);
-
-  React.useEffect(() => {
-    axios
-      .get(`https://hire-job.onrender.com/v1/profile`)
-      .then((response) => {
-        setUserData(response?.data?.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching user data:", error);
-      });
-  }, []);
-
+  
   const handleLogout = () => {
     localStorage.clear();
     window.location.href = "/login";
@@ -49,7 +36,7 @@ function Navigations() {
                 </Link> */}
                 <Dropdown align="end">
                   <Dropdown.Toggle variant="transparent" id="profile-dropdown">
-                    <img src={userData.photo} alt="Profile" />
+                    <img src="/profile_photos/profile2.png" alt="Profile" />
                   </Dropdown.Toggle>
                   <Dropdown.Menu>
                     <Dropdown.Item href="/profile">Profile</Dropdown.Item>
